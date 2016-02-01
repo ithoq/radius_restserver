ba71ddbd9c23feec24019e0405d3a4f9468f7b17
Andrew Yager <andrew@rwts.com.au>
2016-02-01 23:57:07 +1100

    Add test suite code and update codeship-steps.yml to use scripts that wait for db init.

---
+++ src/tests/test-migrate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+RES=`/usr/bin/psql -h db -U postgres postgres -c '\d'`
+RV=$?
+while [ $RV -gt 0 ]
+  do
+  	echo "Still waiting for Postgres database to come up - return value is $RV"
+    sleep 1
+    RES=`/usr/bin/psql -h db -U postgres postgres -c '\d'`
+    RV=$?
+done
+
+./manage.py migrate
+exit $?
